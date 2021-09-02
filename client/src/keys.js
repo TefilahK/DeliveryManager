1137859a68af391d0014d41e44830c6b4899e14a
TefilahK <tefilahkatz@gmail.com>
2021-09-02 21:11:57 +0300

    First commit

--- client/src/keys.js
+++
@@ -1,2 +0,0 @@
-export const  google_API="AIzaSyDcI_g2xEVIlRLbckKNIVZYvs3OVQZWnnU";
-export const  YOUR_ACCESS_TOKEN="pk.5ddce3b149ced23354244159a461c149";
--- server/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = Object.freeze({
-    USER_PASSWORD:'moria555',
-    COOKIE_KEY:"350401be75bbb0fafd3d912a1a1d5e54",
-})
